a951eb57b77e46ee107e91a27d163de14795c29f
Đông <dong.pd1994@outlook.com>
2022-09-24 21:29:13 +0700

    add lesson 5

--- src/class.ts
+++ src/class.ts
@@ -1,45 +1,51 @@
-import HIRAGANA from './hiragana.json';
-import KATAKANA from './katakana.json';
-import LESSON_1 from './lesson1/lesion';
-import LESSON_2 from './lesson2/lesson';
-import LESSON_3 from './lesson3/lesson';
-import LESSON_4 from './lesson4/lesson';
+import HIRAGANA from "./hiragana.json";
+import KATAKANA from "./katakana.json";
+import LESSON_1 from "./lesson1/lesion";
+import LESSON_2 from "./lesson2/lesson";
+import LESSON_3 from "./lesson3/lesson";
+import LESSON_4 from "./lesson4/lesson";
+import LESSON_5 from "./lesson5/lesson";
 
 export const LESSON_JP = {
   ALPHABET: {
     HIRAGANA_ABC: {
       key: "hiragana",
       title: "Hiragana",
-      source: HIRAGANA
+      source: HIRAGANA,
     },
     KATAKANA_ABC: {
       key: "katakana",
       title: "Katakana",
-      source: KATAKANA
-    }
+      source: KATAKANA,
+    },
   },
   LESSON: {
     LESSION_ONE: {
       key: "lesson1",
       title: "Lesson 1",
-      source: LESSON_1
+      source: LESSON_1,
     },
     LESSION_TWO: {
       key: "lesson2",
       title: "Lesson 2",
-      source: LESSON_2
+      source: LESSON_2,
     },
     LESSION_THREE: {
       key: "lesson3",
       title: "Lesson 3",
-      source: LESSON_3
+      source: LESSON_3,
     },
     LESSION_FOUR: {
       key: "lesson4",
       title: "Lesson 4",
-      source: LESSON_4
-    }
-  }
-}
+      source: LESSON_4,
+    },
+    LESSION_FIVE: {
+      key: "lesson5",
+      title: "Lesson 5",
+      source: LESSON_5,
+    },
+  },
+};
 
 export default LESSON_JP;
